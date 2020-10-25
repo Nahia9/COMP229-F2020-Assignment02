323ec1c8c994cf856cf76c116b8d52cebd5e20d6
Nahia Akter <nhaktr@gmail.com>
2020-10-25 15:35:55 -0400

    updated db URI connected to MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,5 @@
 module.exports = {
-    "URI": "mongodb://localhost/contact_list"
+    //"URI": "mongodb://localhost/contact_list"
+
+    "URI": "mongodb+srv://Nahia:1VFNSVkOh5Ujqsm5@mongodbserver.lgdlz.mongodb.net/contact_list?retryWrites=true&w=majority"
 }
